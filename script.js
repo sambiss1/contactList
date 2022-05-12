de163445b6d376533998b91676ef32003b6e5b3f
Sam <sambisselele@gmail.com>
2022-05-12 22:20:22 +0100

    Modification réussie

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const imageReader = new FileReader();
 
 // Uploaded Image(Contact Image)
 const uploadedImageContainer = document.querySelector(".contact__image__container")
-const uploadedImage = document.querySelector("#uploadedImage");
+let uploadedImage = document.querySelector("#uploadedImage");
 
 // Submit button 
 const submitButton = document.querySelector("#submitButton");
@@ -33,23 +33,25 @@ uploadFile.addEventListener("change", (event) => {
   uploadedImage.src = URL.createObjectURL(event.target.files[0]);
 })
 
+let isModified = false;
+console.log(isModified);
 
 /* Submit form */
 form.addEventListener("submit", submitInformation = (event) => {
   event.preventDefault();
+  // if (isModified) return;
   /* Submit contactImage*/
   const contactDetailsImage = document.createElement("img");
-
   /* Create contact details container */
   const contactDetails = document.createElement("div");
   contactDetails.classList.add("contactDetails");
 
   /* Create Contact Image container */
   const contactDetailsImageContainer = document.createElement("div");
-  const firstName = document.createElement("span")
-  const lastName = document.createElement("span");
-  const selectedGroup = document.createElement("p");
-  const bioText = document.createElement("p");
+  let firstName = document.createElement("span")
+  let lastName = document.createElement("span");
+  let selectedOption = document.createElement("p");
+  let bioText = document.createElement("p");
   const iconContainer = document.createElement("span");
   const iconDelete = document.createElement("i");
   iconDelete.classList.add("las", "la-times");
@@ -63,55 +65,125 @@ form.addEventListener("submit", submitInformation = (event) => {
   /* Show Contact Information */
   /* Show image */
   contactDetailsImage.src = uploadedImage.src;
+  console.log(contactDetailsImage.src);
+  // if (uploadedImage.src === "") {
+  //   let errorMessageImage = document.createElement("p");
+  //   errorMessageImage.classList.add("errorMessage");
+  //   errorMessageImage.textContent = "Please upload an image";
+  //   errorMessageImage.style = "color: red";
+  //   uploadedImageContainer.style.display = "block";
+  //   uploadedImageContainer.appendChild(errorMessageImage);
+  // } else {
+  //   contactDetailsImage.src = uploadedImage.src;
+  // }
   /* Show first name*/
   firstName.textContent = firstNameField.value;
   // Show last name
   lastName.textContent = " " + lastNameField.value;
   // Show selected group 
   for (let i = 0; i < selectGroup.length; i++) {
-    selectedGroup.textContent = selectGroup[i].text;
+    selectedOption.textContent = selectGroup[i].text;
   }
   // Show bio
   bioText.textContent = bioField.value;
 
 
-  // Add and show contact Details information
-  contactListContainer.appendChild(contactDetails);
-  // for (let i = 0; i < contactDetails.length; i++) {
+  console.log(isModified);
+  const contactDetailsId = Math.random().toString().substring(2, 17);
+  var contactID = contactDetails.setAttribute("id", contactDetailsId);
+  // Update contact list
+  contactDetails.addEventListener("click", clickOnContactDetails = (event) => {
+    isModified = true;
+    console.log(isModified);
 
-  //   let contactId = 0;
-  //   contactId = contactListContainer.children[i];
-  //   console.log(contactId);
-  // }
+    if (event.target.classList.contains("contactDetails")) {
+      console.log(event.target.id);
+      console.log(contactDetailsId);
+      if (event.target.id === contactDetailsId) {
+        submitButton.value = "Modifier";
+        firstNameField.value = firstName.textContent;
+        console.log(firstNameField.value);
+        lastNameField.value = lastName.textContent;
+        console.log(lastNameField.value);
+        for (let i = 0; i < selectGroup.length; i++) {
+          selectGroup[i].text = selectedOption.textContent;
+        }
+        bioField.value = bioText.textContent;
+        uploadedImage.src = contactDetailsImage.src;
+        console.log(uploadedImage.src);
+        uploadedImageContainer.style.display = "block";
+        uploadedImage.style.display = "block";
 
-  contactDetails.appendChild(contactDetailsImageContainer)
-  contactDetails.appendChild(aboutContact)
+        // Update contact details 
+        submitButton.addEventListener("click", updateContactDetails = (event) => {
+          console.log(firstNameField.value);
+          console.log(lastNameField.value);
+          console.log(firstName.textContent);
+          console.log(lastName.textContent);
+          console.log(contactDetailsImage.src);
+          console.log(uploadedImage.src);
 
-  aboutContact.appendChild(firstName)
-  aboutContact.appendChild(lastName)
-  aboutContact.appendChild(iconContainer)
-  iconContainer.appendChild(iconDelete);
-  aboutContact.appendChild(selectedGroup)
-  aboutContact.appendChild(bioText)
+          event.preventDefault();
+          if (isModified) {
+            firstName.textContent = firstNameField.value;
+            lastName.textContent = " " + lastNameField.value;
+            for (let i = 0; i < selectGroup.length; i++) {
+              selectedOption.textContent = selectGroup[i].text;
+            }
+            bioText.textContent = bioField.value;
+            contactDetailsImage.src = uploadedImage.src;
+            console.log(firstNameField.value);
+            console.log(lastNameField.value);
+            console.log(firstName.textContent);
+            console.log(lastName.textContent);
 
-  contactDetailsImageContainer.appendChild(contactDetailsImage);
+            console.log(contactDetailsImage.src);
+            console.log(uploadedImage.src);
+            submitButton.value = "Créer";
+            isModified = false;
+            console.log(isModified);
+            form.reset();
+          }
+
+        })
+      }
+
+    }
+  });
+
+  /* Create contact list */
+  if (isModified === false) {
+    contactListContainer.appendChild(contactDetails);
+    contactDetails.appendChild(contactDetailsImageContainer)
+    contactDetails.appendChild(aboutContact)
+    aboutContact.appendChild(firstName)
+    aboutContact.appendChild(lastName)
+    aboutContact.appendChild(iconContainer)
+    iconContainer.appendChild(iconDelete);
+    aboutContact.appendChild(selectedOption)
+    aboutContact.appendChild(bioText)
+    contactDetailsImageContainer.appendChild(contactDetailsImage);
+  }
+
+  // Change contact Details on form
+  // Generate unique id for contact details
+
+
+
+  // End of contact details
 
   /* Reset form*/
   form.reset();
   uploadedImageContainer.style.display = "none";
-  uploadedImage.src = ""
-  /*URL.revokeObjectURL(files[0]);*/
-  // Delete contact */
+  uploadedImage.src = "";
+
+  // Delete Contact
   iconDelete.addEventListener("click", deleteInformation = () => {
     contactDetails.remove();
   })
-  // Change Information
-  contactDetails.addEventListener("click", changeInformation = (event) => {
-    submitButton.value = "Modifier"
-    /* Show first name*/
-
-  })
+  // End of delete contact
 })
+
 
 
 
